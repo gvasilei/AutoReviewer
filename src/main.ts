14d215148f43cd0982990a42d08e7543afea9a82
Giorgos Vasileiou <g.vasileiou@gmail.com>
2023-05-01 12:04:14 +0300

    Debug to info

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import {wait} from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
+    core.info(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.info(new Date().toTimeString())
     await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
