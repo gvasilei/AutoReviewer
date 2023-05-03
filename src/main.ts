cb1ca27305a204cd54c502faa3f618424b6312c6
Giorgos Vasileiou <g.vasileiou@gmail.com>
2023-05-03 09:22:10 +0300

    Better logging and integration of octokit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'core-js/actual/structured-clone'
 import { config } from 'dotenv'
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import { wait } from './wait'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
 import {
@@ -15,9 +16,14 @@ config()
 const run = async (): Promise<void> => {
   const repoPath = process.env['GITHUB_WORKSPACE'] || ''
   const runId = process.env['GITHUB_RUN_ID'] || ''
-  const event_name = process.env['GITHUB_EVENT_NAME'] || ''
-  const event_path = process.env['GITHUB_EVENT_PATH'] || ''
+  const eventName = process.env['GITHUB_EVENT_NAME'] || ''
+  const eventPath = process.env['GITHUB_EVENT_PATH'] || ''
   const openAIApiKey = process.env['OPENAI_API_KEY'] || ''
+  const owner = process.env['GITHUB_REPOSITORY_OWNER'] || ''
+  const headRef = process.env['GITHUB_HEAD_REF'] || ''
+  const baseRef = process.env['GITHUB_BASE_REF'] || ''
+  const repoName = process.env['GITHUB_REPOSITORY'] || ''
+  const githubToken = core.getInput('github_token')
 
   const model = new ChatOpenAI({
     temperature: 0,
@@ -25,9 +31,30 @@ const run = async (): Promise<void> => {
     openAIApiKey
   })
 
-  try {
-    core.info(`${repoPath} ${runId} ${event_name} ${event_path}`)
+  const octokit = github.getOctokit(githubToken)
 
+  try {
+    core.info(`${repoPath} ${runId} ${eventName} ${eventPath}`)
+    /*const { data: pullRequest } = await octokit.rest.pulls.get({
+      owner,
+      repo: 'rest.js',
+      pull_number: 123,
+      mediaType: {
+        format: 'diff'
+      }
+    })*/
+
+    const data = await octokit.rest.repos.compareCommitsWithBasehead({
+      basehead: `${baseRef}...${headRef}`,
+      owner,
+      repo: repoName,
+      mediaType: {
+        format: 'diff'
+      }
+    })
+    data.data.files?.map(file => {
+      core.info(`${file.filename} ${file.status}`)
+    })
     // We can also construct an LLMChain from a ChatPromptTemplate and a chat model.
     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
       SystemMessagePromptTemplate.fromTemplate(
@@ -42,17 +69,15 @@ const run = async (): Promise<void> => {
       llm: model
     })
 
-    core.info('set up chain')
-
     const res = await chain.call({
       input_language: 'English',
       output_language: 'French',
       text: 'I love programming.'
     })
 
-    core.info('called chain')
-
-    core.info(res.toString())
+    for (const key in res) {
+      core.info(`${key} - ${res[key]}`)
+    }
 
     const ms: string = core.getInput('milliseconds')
     core.info(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
